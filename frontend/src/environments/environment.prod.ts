cbb65dbc1373fbad67b16e50f8f206c5ec7360ab
Cristopher Miranda <cristopher1012z@gmail.com>
2026-07-08 22:05:37 -0400

    Using Railway now

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
-  apiUrl: 'https://maneja-tus-gastos-api-bf9w.onrender.com',
+  apiUrl: 'https://mod5-univalle-final-project-production.up.railway.app/',
 };
 
