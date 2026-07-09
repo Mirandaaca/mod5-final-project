420e1ea0a0a36cc23ea5970e2a880f4e8e67df95
Cristopher Miranda <cristopher1012z@gmail.com>
2026-07-08 22:08:14 -0400

    deleted a slash unnecessary

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
-  apiUrl: 'https://mod5-univalle-final-project-production.up.railway.app/',
+  apiUrl: 'https://mod5-univalle-final-project-production.up.railway.app',
 };
 
